482a68096d826b0eaf7055cb8efa78619269e3cb
Adelmo Junior <adelmojr07@gmail.com>
2022-06-23 07:50:20 -0300

    add more implementations

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,15 +1,23 @@
+import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
+import 'package:ijob_app/core/model/user_model.dart';
 import 'package:ijob_app/data/remote/user_remote_data_source.dart';
 
 abstract class UserRepository {
-  Future<String> login(String email, String password);
+  Future<Either<Exception, String>> login(String email, String password);
+  Future<Either<Exception, User>> me();
 }
 
 class UserRepositoryImp implements UserRepository {
   final UserRemoteDataSource _remoteDataSource = Get.find(tag: (UserRemoteDataSource).toString());
 
   @override
-  Future<String> login(String email, String password) {
+  Future<Either<Exception, String>> login(String email, String password) {
     return _remoteDataSource.login(email, password);
+  }
+
+  @override
+  Future<Either<Exception, User>> me() {
+    return _remoteDataSource.me();
   }
 }
