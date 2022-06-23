7a69eff1fd05b60fc045f4c6c901cf8fc6aadf21
Adelmo Junior <adelmojr07@gmail.com>
2022-06-23 07:40:41 -0300

    create user model

---
+++ lib/core/model/user_model.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.status,
+    required this.bornDate,
+    required this.about,
+    required this.maxDistance,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.cellPhone,
+    required this.verified,
+    required this.genre,
+    required this.userActiveType,
+  });
+
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final bool status;
+  final DateTime bornDate;
+  final String? about;
+  final int maxDistance;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final String cellPhone;
+  final bool verified;
+  final String genre;
+  final int userActiveType;
+
+  User copyWith({
+    String? id,
+    String? firstName,
+    String? lastName,
+    String? email,
+    bool? status,
+    DateTime? bornDate,
+    String? about,
+    int? maxDistance,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    String? cellPhone,
+    bool? verified,
+    String? genre,
+    int? userActiveType,
+  }) =>
+      User(
+        id: id ?? this.id,
+        firstName: firstName ?? this.firstName,
+        lastName: lastName ?? this.lastName,
+        email: email ?? this.email,
+        status: status ?? this.status,
+        bornDate: bornDate ?? this.bornDate,
+        about: about ?? this.about,
+        maxDistance: maxDistance ?? this.maxDistance,
+        createdAt: createdAt ?? this.createdAt,
+        updatedAt: updatedAt ?? this.updatedAt,
+        cellPhone: cellPhone ?? this.cellPhone,
+        verified: verified ?? this.verified,
+        genre: genre ?? this.genre,
+        userActiveType: userActiveType ?? this.userActiveType,
+      );
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        id: json["id"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        email: json["email"],
+        status: json["status"],
+        bornDate: DateTime.parse(json["bornDate"]),
+        about: json["about"],
+        maxDistance: json["maxDistance"],
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+        cellPhone: json["cellPhone"],
+        verified: json["verified"],
+        genre: json["genre"],
+        userActiveType: json["userActiveType"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email,
+        "status": status,
+        "bornDate": bornDate.toIso8601String(),
+        "about": about,
+        "maxDistance": maxDistance,
+        "createdAt": createdAt.toIso8601String(),
+        "updatedAt": updatedAt.toIso8601String(),
+        "cellPhone": cellPhone,
+        "verified": verified,
+        "genre": genre,
+        "userActiveType": userActiveType,
+      };
+}
