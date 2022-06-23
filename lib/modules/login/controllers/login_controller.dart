8315d94493f53d46cd08b8364d8279a001377b23
Adelmo Junior <adelmojr07@gmail.com>
2022-06-23 07:54:11 -0300

    add login page view

---
+++ lib/modules/login/controllers/login_controller.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ijob_app/core/base/base_controller.dart';
+import 'package:ijob_app/core/widget/snackbar.dart';
+import 'package:ijob_app/data/local/get_storage.dart';
+import 'package:ijob_app/network/exceptions/api_exception.dart';
+
+import '../../../data/repositories/user_repository.dart';
+import '../../../routes/app_pages.dart';
+
+class LoginController extends BaseController {
+  final headerImages = ['assets/BannerLogin1.png', 'assets/BannerLogin2.png'];
+  final UserRepository _userRepository = Get.find(tag: (UserRepository).toString());
+
+  final _passwordHasVisible = false.obs;
+  final _emailTextController = TextEditingController().obs;
+  final _passwordTextController = TextEditingController().obs;
+
+  TextEditingController get emailTextController => _emailTextController.value;
+  TextEditingController get passwordTextController => _passwordTextController.value;
+
+  bool get passwordHasVisible => _passwordHasVisible.value;
+  set passwordHasVisible(bool value) => _passwordHasVisible.value = value;
+
+  void login() async {
+    showLoading();
+    final loggedOrError =
+        await _userRepository.login(_emailTextController.value.text, _passwordTextController.value.text);
+
+    loggedOrError.fold((error) {
+      if (error is ApiException) {
+        switch (error.httpCode) {
+          case 400:
+            showRedSnackBar('Dados inválidos', 'E-mail ou senha incorreto');
+            break;
+          case 404:
+            showRedSnackBar('Dados inválidos', 'Usuário não encontrado');
+            break;
+        }
+      } else {
+        showRedSnackBar(
+          'Erro',
+          'Não foi possível realizar login, tente novamente mais tarde',
+        );
+      }
+    }, (token) async {
+      final localStorage = LocalStorageImp();
+      localStorage.writeToken(token);
+      final userOrError = await _userRepository.me();
+      userOrError.fold((error) => null, (user) {
+        Get.offAllNamed(
+          user.userActiveType == 0 ? Routes.homeEmployee : Routes.homeEmployer,
+        );
+      });
+    });
+    resetPageState();
+  }
+
+  //validators
+  bool get emailIsValid => _emailTextController.value.text.isEmail;
+  bool get passwordIsValid => _passwordTextController.value.text.isNotEmpty;
+
+  bool get formIsValid => emailIsValid && passwordIsValid;
+}
---
+++ lib/modules/login/views/login_view.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ijob_app/core/base/base_view.dart';
+import 'package:ijob_app/core/values/app_colors.dart';
+import 'package:ijob_app/core/values/app_values.dart';
+import 'package:ijob_app/core/values/text_styles.dart';
+import 'package:ijob_app/core/widget/snackbar.dart';
+import 'package:ijob_app/modules/login/controllers/login_controller.dart';
+
+class LoginView extends BaseView<LoginController> {
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        color: AppColors.blackColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: AppValues.margin_20),
+              height: 300,
+              width: double.maxFinite,
+              child: PageView.builder(
+                  itemCount: controller.headerImages.length,
+                  pageSnapping: true,
+                  itemBuilder: (context, pagePosition) {
+                    return Image.asset(
+                      controller.headerImages[pagePosition],
+                      fit: BoxFit.cover,
+                    );
+                  }),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(AppValues.radius_25),
+                  topRight: Radius.circular(AppValues.radius_25),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Login',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: controller.emailTextController,
+                      style: blackText18,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 15),
+                    Obx(
+                      () => TextFormField(
+                        controller: controller.passwordTextController,
+                        style: blackText18,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          suffixIcon: GestureDetector(
+                            onTap: () => controller.passwordHasVisible =
+                                !controller.passwordHasVisible,
+                            child: Icon(
+                              controller.passwordHasVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                          ),
+                        ),
+                        obscureText: !controller.passwordHasVisible,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // go to forgot password
+                      },
+                      child: const Text(
+                        'Esqueci minha senha',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          // go to home employee
+                          controller.formIsValid
+                              ? controller.login()
+                              : showRedSnackBar(
+                                  'Erro',
+                                  'Existe campos inválidos',
+                                );
+                        },
+                        child: const Text('Continuar', style: yellowText35),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset('assets/loginGoogle.png', height: 75),
+                        Image.asset('assets/loginFacebook.png', height: 75),
+                        Image.asset('assets/loginLinkedin.png', height: 75),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Novo aqui?',
+                            style: TextStyle(
+                                fontStyle: FontStyle.italic, fontSize: 17),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              // go to register page
+                            },
+                            child: const Text(
+                              'Crie uma conta!',
+                              style: TextStyle(
+                                  color: Colors.amberAccent,
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 17),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
