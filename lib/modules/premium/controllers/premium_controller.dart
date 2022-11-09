7edd45530a17db00c2dd22eb940ce54975078a24
Adelmo Junior <adelmo.junior@solides.com.br>
2022-11-09 19:14:30 -0300

    add exception catch on get plans

--- lib/modules/premium/controllers/premium_controller.dart
+++ lib/modules/premium/controllers/premium_controller.dart
@@ -3,15 +3,11 @@ import 'package:ijob_app/core/base/base_controller.dart';
 import 'package:ijob_app/core/model/plan_model.dart';
 import 'package:ijob_app/core/widget/snackbar.dart';
 import 'package:ijob_app/data/repositories/plan_repository.dart';
+import 'package:ijob_app/network/exceptions/not_found_exception.dart';
 
 class PremiumnController extends BaseController {
   final PlanRepository _planRepository = Get.find(tag: (PlanRepository).toString());
-  final assetsCarousel = <String>[
-    'assets/BannerPremium1.png',
-    'assets/BannerPremium2.png',
-    'assets/BannerPremium3.png',
-    'assets/BannerPremium4.png'
-  ].obs;
+  final assetsCarousel = <String>['assets/BannerPremium1.png', 'assets/BannerPremium2.png', 'assets/BannerPremium3.png', 'assets/BannerPremium4.png'].obs;
 
   final _plans = <Plan>[].obs;
   final Rx<Plan?> _selectedPlan = (null as Plan?).obs;
@@ -23,7 +19,13 @@ class PremiumnController extends BaseController {
     showLoading();
     final errorOrPlans = await _planRepository.getPlans();
     errorOrPlans.fold(
-      (error) => showRedSnackBar('Erro', 'Não foi possível obter os planos, tente novamente mais tarde'),
+      (error) {
+        if (error is NotFoundException) {
+          showGreenSnackBar('Planos', 'Não existe planos cadastrados');
+        } else {
+          showRedSnackBar('Erro', 'Não foi possível obter os planos, tente novamente mais tarde');
+        }
+      },
       (plans) => _plans.addAll(plans),
     );
     hideLoading();
