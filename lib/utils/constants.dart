0a7a9aa2f949067b3e7292ea6f316564d39b846f
Adelmo Junior <adelmo.junior@solides.com.br>
2022-11-16 17:44:10 -0300

    bug fix

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,6 +4,6 @@ enum UserActiveType {
 }
 
 class Utils {
-  static const String baseUrl = 'https://192.168.1.7:3000';
+  static const String baseUrl = 'https://191.5.55.11:3000';
   static const String defaultAvatarUrl = 'https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true';
 }
