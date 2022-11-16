d71710d9cd78879a1198068345becf6d54e6fdc3
Adelmo Junior <adelmo.junior@solides.com.br>
2022-11-16 19:50:50 -0300

    remove material

--- lib/modules/Documentção/documentacao.dart
+++ lib/modules/Documentção/documentacao.dart
@@ -11,135 +11,122 @@ class _documentacao extends State<documentacao> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          title: const Text(
-            'Documentação e Segurança',
-            style: TextStyle(fontSize: 20, color: Colors.black),
-          ),
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          'Documentação e Segurança',
+          style: TextStyle(fontSize: 20, color: Colors.black),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 5, 30, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Dados opcionais',
-                    style:
-                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 10),
-                const Text(
-                  '   Alguns dos seus dados podem ser fornecidos opcionalmente, assim aprimoramos a segurança de todos e você ainda é melhor recomendado para os trabalhos',
-                  style: TextStyle(color: Colors.grey, fontSize: 17),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  '   Toque na imagem para adicionar uma foto',
-                  style: TextStyle(fontSize: 15),
-                ),
-                Container(
-                    height: 170,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(80, 30, 80, 30),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.transparent,
-                        ),
-                      ),
-                    )),
-                const SizedBox(height: 20),
-                const Text(
-                  'Trocar senha',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                TextField(
-                  style: const TextStyle(fontSize: 20),
-                  decoration: InputDecoration(
-                      labelText: 'Senha atual',
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _obscureTextAtual = !_obscureTextAtual;
-                          });
-                        },
-                        child: Icon(_obscureTextAtual
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                      )),
-                  obscureText: _obscureTextAtual,
-                ),
-                TextField(
-                  style: const TextStyle(fontSize: 20),
-                  decoration: InputDecoration(
-                      labelText: 'Nova senha',
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _obscureTextNova = !_obscureTextNova;
-                          });
-                        },
-                        child: Icon(_obscureTextNova
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                      )),
-                  obscureText: _obscureTextNova,
-                ),
-                TextField(
-                  style: const TextStyle(fontSize: 20),
-                  decoration: InputDecoration(
-                      labelText: 'Nova senha novamente',
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _obscureTextNova = !_obscureTextNova;
-                          });
-                        },
-                        child: Icon(_obscureTextNova
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                      )),
-                  obscureText: _obscureTextNova,
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: SizedBox(
-                    height: 40,
-                    width: 270,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          openConfirmadoSenha();
-                        },
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Colors.amberAccent),
-                            shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)))),
-                        child: const Text(
-                          'Confirmar nova senha',
-                          style: TextStyle(fontSize: 20, color: Colors.black),
-                        )),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 5, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Dados opcionais', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              const Text(
+                '   Alguns dos seus dados podem ser fornecidos opcionalmente, assim aprimoramos a segurança de todos e você ainda é melhor recomendado para os trabalhos',
+                style: TextStyle(color: Colors.grey, fontSize: 17),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                '   Toque na imagem para adicionar uma foto',
+                style: TextStyle(fontSize: 15),
+              ),
+              Container(
+                  height: 170,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
                   ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(80, 30, 80, 30),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  )),
+              const SizedBox(height: 20),
+              const Text(
+                'Trocar senha',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                style: const TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                    labelText: 'Senha atual',
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscureTextAtual = !_obscureTextAtual;
+                        });
+                      },
+                      child: Icon(_obscureTextAtual ? Icons.visibility : Icons.visibility_off),
+                    )),
+                obscureText: _obscureTextAtual,
+              ),
+              TextField(
+                style: const TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                    labelText: 'Nova senha',
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscureTextNova = !_obscureTextNova;
+                        });
+                      },
+                      child: Icon(_obscureTextNova ? Icons.visibility : Icons.visibility_off),
+                    )),
+                obscureText: _obscureTextNova,
+              ),
+              TextField(
+                style: const TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                    labelText: 'Nova senha novamente',
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscureTextNova = !_obscureTextNova;
+                        });
+                      },
+                      child: Icon(_obscureTextNova ? Icons.visibility : Icons.visibility_off),
+                    )),
+                obscureText: _obscureTextNova,
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: SizedBox(
+                  height: 40,
+                  width: 270,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        openConfirmadoSenha();
+                      },
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)))),
+                      child: const Text(
+                        'Confirmar nova senha',
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      )),
                 ),
-                const SizedBox(height: 20),
-                /*Row(
+              ),
+              const SizedBox(height: 20),
+              /*Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     const Text(
@@ -161,8 +148,7 @@ class _documentacao extends State<documentacao> {
                   'Essa funcionalidade permite que você adicione uma camada extra de segurança na sua conta, depois de ativada, toda vez que um novo aparelho tentar entrar na sua conta um código será solicitado.',
                   style: TextStyle(color: Colors.grey),
                 )*/
-              ],
-            ),
+            ],
           ),
         ),
       ),
@@ -187,11 +173,8 @@ class _documentacao extends State<documentacao> {
                 const SizedBox(height: 15),
                 ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.amberAccent),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
                   ),
                   onPressed: () {
                     Navigator.pop(context);
--- lib/modules/Suporte/feedback.dart
+++ lib/modules/Suporte/feedback.dart
@@ -8,89 +8,82 @@ class feedback extends StatefulWidget {
 class _feedback extends State<feedback> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          title: const Text(
-            'Feedback',
-            style: TextStyle(fontSize: 30, color: Colors.black),
-          ),
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          'Feedback',
+          style: TextStyle(fontSize: 30, color: Colors.black),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              children: [
-                const TextField(
-                    decoration: InputDecoration(labelText: 'Nome'),
-                    style: TextStyle(fontSize: 20)),
-                const SizedBox(height: 20),
-                const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                    ),
-                    style: TextStyle(fontSize: 20)),
-                const SizedBox(height: 20),
-                const SizedBox(
-                  height: 200,
-                  width: double.infinity,
-                  child: TextField(
-                    maxLines: null,
-                    expands: true,
-                    decoration: InputDecoration(
-                      labelText: 'Descrição...',
-                      border: OutlineInputBorder(),
-                    ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const TextField(decoration: InputDecoration(labelText: 'Nome'), style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 20),
+              const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 20),
+              const SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: TextField(
+                  maxLines: null,
+                  expands: true,
+                  decoration: InputDecoration(
+                    labelText: 'Descrição...',
+                    border: OutlineInputBorder(),
+                  ),
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: const BorderRadius.all(Radius.circular(5)),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Toque aqui para enviar\n    fotos ou arquivos.',
                     style: TextStyle(fontSize: 20),
                   ),
                 ),
-                const SizedBox(height: 20),
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: const BorderRadius.all(Radius.circular(5)),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 40,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    openEmailEnviado();
+                  },
+                  child: const Text(
+                    'Enviar',
+                    style: TextStyle(fontSize: 20, color: Colors.black),
                   ),
-                  child: const Center(
-                    child: Text(
-                      'Toque aqui para enviar\n    fotos ou arquivos.',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                SizedBox(
-                  height: 40,
-                  width: 150,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      openEmailEnviado();
-                    },
-                    child: const Text(
-                      'Enviar',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.amberAccent),
-                    ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
@@ -115,11 +108,8 @@ class _feedback extends State<feedback> {
                 const SizedBox(height: 10),
                 ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.amberAccent),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
                   ),
                   onPressed: () {
                     Navigator.pop(context);
